9c8c0dd1227d2223b6ad8d2947997a209734ebe1
divitrao <divitrao97d@gmail.com>
2021-02-19 16:36:51 +0530

    added buttons to increase and decrese buying quantity and added some product pictures

--- invoice/product/views.py
+++ invoice/product/views.py
@@ -7,15 +7,20 @@ def home(request):
 #     return render(request,'order_page.html')
 
 def orderPage(request):
-    if request.method== 'POST':
-        global form1, form2,form3
-        form1= request.POST['inkPens']
-        form2 = request.POST['ballpoint-pen']
-        form3 = request.POST['pilot-pen']
-        # print(form1)
-        # print(form2)
-        # print(form3)
-        return render(request,'payments.html')
+    if request.method == 'POST':
+        action = request.POST.get('submission')
+        if action == 'only_submit':
+            global form1, form2, form3
+            form1 = request.POST['inkPens']
+            form2 = request.POST['ballpoint-pen']
+            form3 = request.POST['pilot-pen']
+            # print(form1)
+            # print(form2)
+            # print(form3)
+            return render(request, 'payments.html')
+
+
+
 
     else:
         return render(request,'order_page.html')
